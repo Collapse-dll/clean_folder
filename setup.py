c19e29efd27e0d5169a7303a3138753825823aad
Collapse-dll <rassskoll@gmail.com>
2023-11-01 10:43:00 +0100

    ver=0.1.1

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='clean',
-      version='0.1.0',
-      author='Saitov Mykyta')
+      version='0.1.1',
+      packages=find_packages(),
+      entry_points = {'console_scripts': ['clean = clean_folder.clean:main']},
+      author='Saitov Mykyta'
+      )
